a73e626e40225e6e64a3fca985b766179616b5ee
Moisés David Jiménez Ortiz <143739485+MJoDev@users.noreply.github.com>
2024-05-28 21:35:30 -0400

    Tile

--- app/components/ChessBoard/ChessBoard.tsx
+++ app/components/ChessBoard/ChessBoard.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 import './Chessboard.css';
+import Tile from '../Tile/Tile'
+
+interface Piece{
+    image: string;
+    x: number;
+    y: number;
+}
+
+const pieces: Piece[] = [];
 
 const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"]
 const horizontalAxis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
@@ -11,15 +20,14 @@ export default function ChessBoard(){
     for(let i = verticalAxis.length - 1; i >= 0; i--){
 
         for(let j = 0; j<horizontalAxis.length; j++){
-
+            let image = undefined;
             const number = j + i;
-            if(number % 2 === 0){
-                board.push(<div className="tile black-tile"> [{horizontalAxis[j]}{verticalAxis[i]}]</div>)
-            }else {
-                board.push(<div className="tile white-tile"> [{horizontalAxis[j]}{verticalAxis[i]}]</div>)
 
-            }
+            pieces.forEach(p => {
+                
+            });
 
+            board.push(<Tile number={number} image={image}/>)
            
         }
     }
