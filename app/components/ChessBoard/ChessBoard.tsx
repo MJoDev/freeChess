cf08e86ec62bfe000e56d04dc4be09d9dd32b67f
Moisés David Jiménez Ortiz <143739485+MJoDev@users.noreply.github.com>
2024-05-29 10:41:18 -0400

    Pawns
    Added pawns

--- app/components/ChessBoard/ChessBoard.tsx
+++ app/components/ChessBoard/ChessBoard.tsx
@@ -13,6 +13,15 @@ const pieces: Piece[] = [];
 const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"]
 const horizontalAxis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 
+for(let i = 0; i < 8; i++){
+    pieces.push({image: 'icons/BlackPawn.png', x:i, y:6});
+}
+for(let i = 0; i < 8; i++){
+    pieces.push({image: 'icons/WhitePawn.png', x:i, y:1});
+}
+
+
+
 export default function ChessBoard(){
 
     let board = [];
@@ -24,7 +33,9 @@ export default function ChessBoard(){
             const number = j + i;
 
             pieces.forEach(p => {
-                
+                if(p.x === j && p.y === i){
+                    image = p.image;
+                }
             });
 
             board.push(<Tile number={number} image={image}/>)
