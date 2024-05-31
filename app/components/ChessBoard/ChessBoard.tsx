a066bc5858baf84c94022823014e48a46adb5d3a
Moisés David Jiménez Ortiz <143739485+MJoDev@users.noreply.github.com>
2024-05-30 21:01:31 -0400

    drop pieces

--- app/components/ChessBoard/ChessBoard.tsx
+++ app/components/ChessBoard/ChessBoard.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import './Chessboard.css';
 import Tile from '../Tile/Tile'
 
@@ -15,53 +15,64 @@ let activePiece: HTMLElement | null = null;
 
 const pieces: Piece[] = [];
 
+const initialBoardState: Piece[] = []
+
+for(let i = 0; i < 8; i++){
+    initialBoardState.push({image: 'icons/BlackPawn.png', x:i, y:6});
+}
+for(let i = 0; i < 8; i++){
+    initialBoardState.push({image: 'icons/WhitePawn.png', x:i, y:1});
+}
 for(let p = 0; p<2; p++){
     const type = (p === 0) ? "Black" : "White";
     const y = (p === 0) ? 7 : 0;
 
-    pieces.push({image: `icons/${type}Rook.png`, x:0, y})
-    pieces.push({image: `icons/${type}Rook.png`, x:7, y})
-    pieces.push({image: `icons/${type}Knight.png`, x:1, y})
-    pieces.push({image: `icons/${type}Knight.png`, x:6, y})
-    pieces.push({image: `icons/${type}Bishop.png`, x:2, y})
-    pieces.push({image: `icons/${type}Bishop.png`, x:5, y})
-    pieces.push({image: `icons/${type}King.png`, x:4, y})
-    pieces.push({image: `icons/${type}Queen.png`, x:3, y})
+    initialBoardState.push({image: `icons/${type}Rook.png`, x:0, y})
+    initialBoardState.push({image: `icons/${type}Rook.png`, x:7, y})
+    initialBoardState.push({image: `icons/${type}Knight.png`, x:1, y})
+    initialBoardState.push({image: `icons/${type}Knight.png`, x:6, y})
+    initialBoardState.push({image: `icons/${type}Bishop.png`, x:2, y})
+    initialBoardState.push({image: `icons/${type}Bishop.png`, x:5, y})
+    initialBoardState.push({image: `icons/${type}King.png`, x:4, y})
+    initialBoardState.push({image: `icons/${type}Queen.png`, x:3, y})
 }
 
 const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"]
 const horizontalAxis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 
-for(let i = 0; i < 8; i++){
-    pieces.push({image: 'icons/BlackPawn.png', x:i, y:6});
-}
-for(let i = 0; i < 8; i++){
-    pieces.push({image: 'icons/WhitePawn.png', x:i, y:1});
-}
-
 export default function ChessBoard(){
-
+    const [activePiece, setActivePiece] = useState<HTMLElement | null>(null);
+    const [gridX, setGridX] = useState(0);
+    const [gridY, setGridY] = useState(0);
     const chessBoardRef = useRef<HTMLDivElement>(null);
+    const [pieces, setPieces] = useState<Piece[]>(initialBoardState)
+
+    useEffect(() => {
+       
+    })
 
     let board = [];
 
     function grabPiece(e: React.MouseEvent){
+        const chessboard = chessBoardRef.current;
         const element = e.target as HTMLElement;
     
-        if(element.classList.contains("chess-piece")){
-    
+        if(element.classList.contains("chess-piece") && chessboard){
+            
+            setGridX(Math.floor((e.clientX - chessboard.offsetLeft) / 75));
+            setGridY(Math.abs(Math.ceil((e.clientY - chessboard.offsetTop -600) / 75)))
             const x = e.clientX -35;
             const y = e.clientY -35;
             element.style.position = 'absolute';
             element.style.left = `${x}px`;
             element.style.top = `${y}px`;
     
-            activePiece = element;
+            setActivePiece(element)
         }
     
     }
     function movePiece(e: React.MouseEvent) {
-
+        
         const chessboard = chessBoardRef.current;
 
         if(activePiece && chessboard){
@@ -93,8 +104,24 @@ export default function ChessBoard(){
         }
     }
     function dropPiece(e: React.MouseEvent){
-        if(activePiece){
-            activePiece = null;
+
+        const chessboard = chessBoardRef.current;
+
+        if(activePiece && chessboard){
+            setPieces(value => {
+                const x = Math.floor((e.clientX - chessboard.offsetLeft) / 75)
+                const y = Math.abs(Math.ceil((e.clientY - chessboard.offsetTop -600) / 75))
+                const pieces = value.map(p=>{
+                    if(p.x === gridX && p.y=== gridY){
+                        p.x = x;
+                        p.y = y;
+                        console.log(x, y)
+                    }
+                    return p;
+                })
+                return pieces;
+            })
+            setActivePiece(null)
         }
     }
 
--- app/play/page.tsx
+++ app/play/page.tsx
@@ -1,9 +1,11 @@
-import ChessBoard from "../components/ChessBoard/ChessBoard"
+
 import '../app.css'
+import ChessBoard from '../components/ChessBoard/ChessBoard'
+
 
 export default function HomePage(){
     return (<div className="app">
-        <h1>FreeChess.com HOLA</h1>
+        <h1>FreeChess.com</h1>
         <ChessBoard></ChessBoard>
     </div>)
 }
